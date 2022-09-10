c562784121c9b5534674b62d0773c8c28ee4e4eb
tomasrajao <t-tomas.rajao@cnidis.com.br>
2022-09-10 16:48:24 -0300

    Criado modelo de tarefas resolve #3

--- todo_app/tasks/admin.py
+++ todo_app/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from todo_app.tasks.models import Task
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'truncated_description',
+        'active',
+        'modified_at',
+        'registered_at',
+    )
+
---
+++ todo_app/tasks/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Título')
+    description = models.TextField(verbose_name='Descrição', null=True, blank=True)
+    active = models.BooleanField(verbose_name='Ativo', default=True)
+    modified_at = models.DateTimeField(verbose_name='Modificado em', auto_now=True)
+    registered_at = models.DateTimeField(verbose_name='Registrado em', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def truncated_description(self):
+        if len(self.description) > 30:
+            return f'{self.description[:30]}...'
+        return self.description
+    truncated_description.short_description = 'Descrição'
