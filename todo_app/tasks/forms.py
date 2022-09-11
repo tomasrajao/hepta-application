bf827d152a69e2a331563a215b57e0df5f0b7af5
tomasrajao <t-tomas.rajao@cnidis.com.br>
2022-09-11 02:38:30 -0300

    Criado adicionar tarefas resolve #7

--- todo_app/tasks/forms.py
+++ todo_app/tasks/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 
+from todo_app.tasks.models import Task
 
-class TaskForm(forms.Form):
+
+class TaskForm(forms.ModelForm):
     title = forms.CharField(label='Título', max_length=300)
     description = forms.CharField(
         label='Descrição', widget=forms.Textarea)
+
+    class Meta:
+        model = Task
+        fields = ['title', 'description']
--- todo_app/tasks/urls.py
+++ todo_app/tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
-    path('list-tasks/', views.list_tasks, name='list-tasks')
+    path('list-tasks/', views.list_tasks, name='list-tasks'),
+    path('add-tasks/', views.add_tasks, name='add-tasks')
 ]
