6e552a65a274b2f8a89a24f8e78514b525a13e01
tomasrajao <t-tomas.rajao@cnidis.com.br>
2022-09-11 02:38:30 -0300

    Criado adicionar tarefas resolve #7

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from todo_app.tasks.forms import TaskForm
 from todo_app.tasks.models import Task
 
 
 def list_tasks(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/list_tasks.html', context={'tasks': tasks})
+
+
+def add_tasks(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/add_task.html', {'form': form})
