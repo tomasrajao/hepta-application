1ff53e61530a484232e7376b6247617d52c49a19
tomasrajao <t-tomas.rajao@cnidis.com.br>
2022-09-11 01:22:37 -0300

    Criada listagem de tarefas resolve #6

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from todo_app.tasks.models import Task
+
+
+def list_tasks(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list_tasks.html', context={'tasks': tasks})
