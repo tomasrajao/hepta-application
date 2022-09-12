de01d6069268144219828d46f7320ec1061adfe9
tomasrajao <t-tomas.rajao@cnidis.com.br>
2022-09-12 05:58:03 -0300

    Implementando layout com Bootstrap

--- todo_app/tasks/forms.py
+++ todo_app/tasks/forms.py
@@ -10,4 +10,4 @@ class TaskForm(forms.ModelForm):
 
     class Meta:
         model = Task
-        fields = ['title', 'description', 'active']
+        fields = ['title', 'description', 'contact', 'active',]
